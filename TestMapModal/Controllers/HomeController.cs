f96fb314078bada04d5c7811af689bbfd1b003d0
Jackson Hales <me@jacksonhales.com.au>
2018-11-08 21:22:18 +1100

    Initial Commit

--- TestMapModal/Controllers/HomeController.cs
+++ TestMapModal/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestMapModal.Models;
 
 namespace TestMapModal.Controllers
 {
     public class HomeController : Controller
     {
+
+        Section section = new Section();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult partialviewme()
+        {
+            section.Name = "John";
+
+            return PartialView(section);
         }
 
         public ActionResult About()
---
+++ TestMapModal/Models/Section.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMapModal.Models
+{
+    public class Section
+    {
+        public string Name { get; set; }
+    }
+}
